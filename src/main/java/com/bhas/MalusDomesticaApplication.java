67efc389fa7962910408b9660700cfa8d5ec83a5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-16 13:50:44 +0530

    <excludes> 						<exclude> 							<groupId>org.projectlombok</groupId> 							<artifactId>lombok</artifactId> 						</exclude> 					</excludes>

--- src/main/java/com/bhas/MalusDomesticaApplication.java
+++ src/main/java/com/bhas/MalusDomesticaApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MalusDomesticaApplication {
+public class MalusDomesticaApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(MalusDomesticaApplication.class, args);
 	}
 
