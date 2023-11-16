028db7fc9b3d659e0d3bb7bed148375bcd5b76e0
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-16 15:55:28 +0530

    private AppleService appleService;

--- src/main/java/com/bhas/controller/AppleController.java
+++ src/main/java/com/bhas/controller/AppleController.java
@@ -1,9 +1,17 @@
 package com.bhas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.bhas.service.AppleService;
+
+@RestController("/apple")
 public class AppleController 
 {
-
+	// DI,
+	@Autowired
+	private AppleService appleService;
+	
+	// API's
+	
 }
--- src/main/java/com/bhas/service/AppleService.java
+++ src/main/java/com/bhas/service/AppleService.java
@@ -2,8 +2,6 @@ package com.bhas.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bhas.dto.AppleDTO;
 import com.bhas.modal.Apple;
@@ -17,15 +15,13 @@ public class AppleService
 	private AppleRepo appleRepo;
 	
 	// CRUD operations,
-	// 1. Get for receiving the data,
-	@GetMapping("/wish")
+	// 1. test EndPoint,s
 	public String greet()
 	{
 		return "Welcome to the Malus_App!,";
 	}
 	
-	// 2.Post for sending the data,
-	@PostMapping("/insertApl")
+	// 2.INSERT INTO TABLE-NAME VALUES();
 	public Apple insertAppl(AppleDTO aplDTO)
 	{
 		Apple ap = new Apple(aplDTO);
@@ -33,10 +29,27 @@ public class AppleService
 	}
 	
 	// 3. SELECT * FROM TABLE-NAME WHERE id = " ";
-	@GetMapping("/getApl/{id}")
 	public Apple getApl(Integer aplId)
 	{
 		return appleRepo.findById(aplId).get();
+	}
+	
+	// 4. UPDATE
+	public Apple updateApl(Integer aplId,AppleDTO aplAppleDTO)
+	{
+		Apple ap = appleRepo.findById(aplId).get();
+				ap.setAplName(aplAppleDTO.getAplName());
+				ap.setAplPrice(aplAppleDTO.getAplPrice());
+				ap.setAplQty(aplAppleDTO.getAplQty());
+				
+				return appleRepo.save(ap);
+	}
+	
+	// 5. DELETE
+	public String deleteApl(Integer aplId)
+	{
+		appleRepo.deleteById(aplId);
+		return " ";
 	}
 	
 }
