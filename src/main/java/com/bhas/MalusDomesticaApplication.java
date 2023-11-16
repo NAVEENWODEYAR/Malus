63597d5da539ebbbb1fdad2946a911a6c40ebba4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-16 13:45:57 +0530

    initial commit

---
+++ src/main/java/com/bhas/MalusDomesticaApplication.java
@@ -0,0 +1,13 @@
+package com.bhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalusDomesticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalusDomesticaApplication.class, args);
+	}
+
+}
