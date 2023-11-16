43b2949894b336175268b1b5ae1e8a1a30157790
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-16 15:29:24 +0530

    @GeneratedValue(strategy = GenerationType.AUTO)

--- src/main/java/com/bhas/modal/Apple.java
+++ src/main/java/com/bhas/modal/Apple.java
@@ -1,6 +1,9 @@
 package com.bhas.modal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +16,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "")
 public class Apple 
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int aplId;
 	
 	private String aplName;
