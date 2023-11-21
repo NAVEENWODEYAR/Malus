24ebedf1b6b912b2d415b6cf425c6f6d97438e3d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-21 23:52:41 +0530

    @Repository

--- src/main/java/com/bhas/repo/AppleRepo.java
+++ src/main/java/com/bhas/repo/AppleRepo.java
@@ -1,9 +1,11 @@
 package com.bhas.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bhas.modal.Apple;
 
+@Repository
 public interface AppleRepo extends JpaRepository<Apple, Integer>
 {
 
