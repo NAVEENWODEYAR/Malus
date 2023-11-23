c6d115fed7724bf9f59f70d4e971e4f0533572bf
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-23 13:47:27 +0530

    mappings in entity

--- src/main/java/com/bhas/modal/Dealer.java
+++ src/main/java/com/bhas/modal/Dealer.java
@@ -1,6 +1,9 @@
 package com.bhas.modal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +16,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "Apple-Vendor")
 public class Dealer 
 {
+	@Id
+	@GeneratedValue (strategy = GenerationType.SEQUENCE)
 	private int dId;
 	private String dName;
 	private String ddddress;
