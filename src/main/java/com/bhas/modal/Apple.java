89c9c022fa1948ed79793933755e654bd58313c4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-23 16:23:37 +0530

    @OneToMany(cascade = CascadeType.ALL)

--- src/main/java/com/bhas/modal/Apple.java
+++ src/main/java/com/bhas/modal/Apple.java
@@ -1,13 +1,17 @@
 package com.bhas.modal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.bhas.dto.AppleDTO;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,7 +31,9 @@ public class Apple
 	private double aplPrice;
 	private String aplQty;
 	
-	private List<Dealer> dealers;
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "dId")
+	private List<Dealer> dealers = new ArrayList<>();
 	
 	public Apple(AppleDTO aplDto)
 	{
