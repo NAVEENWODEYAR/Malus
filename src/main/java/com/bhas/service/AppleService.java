bc6d6c0b3aff09dddb0869a654bbdce4a076700c
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2026-08-22 21:54:07 +0530

    Update AppleService.java

--- src/main/java/com/bhas/service/AppleService.java
+++ src/main/java/com/bhas/service/AppleService.java
@@ -22,7 +22,7 @@ public class AppleService
 	public Apple insertAppl(AppleDTO aplDTO)
 	{
 		Apple ap = new Apple(aplDTO);
-System.out.println("Insert new record into database");
+System.out.println("Inserting new record into database");
 		return appleRepo.save(ap);
 	}
 	
@@ -55,7 +55,7 @@ System.out.println("Insert new record into database");
 	public String deleteApl(Integer aplId)
 	{
 		appleRepo.deleteById(aplId);
-System.out.println("Deleted the record with Id "+aplId+");
+System.out.println("Deleted the record from table with Id "+aplId+");
 		return "Deletion successfull";
 	}
 	
